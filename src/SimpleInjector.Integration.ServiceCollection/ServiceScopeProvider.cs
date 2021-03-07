8b51e0cac3dcd2da8673f8c58b04f70d67db15a4
dotnetjunkie <steven@cuttingedge.it>
2021-03-07 16:37:32 +0100

    Moved ServiceScopeProvider to a different namespace. #14

--- src/SimpleInjector.Integration.ServiceCollection/ServiceScopeProvider.cs
+++ src/SimpleInjector.Integration.ServiceCollection/ServiceScopeProvider.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Simple Injector Contributors. All rights reserved.
 // Licensed under the MIT License. See LICENSE file in the project root for license information.
 
-namespace SimpleInjector
+namespace SimpleInjector.Integration.ServiceCollection
 {
     using Microsoft.Extensions.DependencyInjection;
 
